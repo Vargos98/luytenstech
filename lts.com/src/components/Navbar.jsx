54aec40b453c707d020d98939fd97b2a8d697558
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-09-27 18:01:09 +0530

    Started work on Lts

---
+++ lts.com/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+import Logo from '../assets/Logo_Luytens.png';
+const Navbar = () => {
+  return (
+    <div className="max-w-screen-xl py-6 bg-zinc-800 mx-auto">
+      <img src={Logo} alt="" />
+    </div>
+  )
+}
+
+export default Navbar
