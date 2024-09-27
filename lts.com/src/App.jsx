90aefdeacb42742ad99c1024bdb011d9b778ff17
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-09-27 18:35:21 +0530

    Navbar basic layout done

--- lts.com/src/App.jsx
+++ lts.com/src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-    <div className="w-full h-screen bg-zinc-700 text-white">
+    <div className="w-full h-screen bg-zinc-900 text-white">
     <Navbar/>
     </div>
   )
