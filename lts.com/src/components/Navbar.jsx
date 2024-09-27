6326415298fee132e487c5fce62a82faf88d3062
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-09-27 18:51:02 +0530

    Navbar Completed

--- lts.com/src/components/Navbar.jsx
+++ lts.com/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 // import React from 'react'
 import Logo from '../assets/Logo_Luytens.png';
+import { Button } from './Button';
+
 const Navbar = () => {
   return (
-    <div className="max-w-screen-xl py-6 bg-zinc-900 mx-auto flex items-center justify-between">
+    <div className="max-w-screen-xl py-6 bg-zinc-900 mx-auto flex items-center justify-between border-b-[1px] border-zinc-700">
       <div className='flex items-center'>
         <img className="w-20" src={Logo} alt="" />
         <p className='text-2xl'>Luytens Technology Solutions</p>
@@ -14,6 +16,9 @@ const Navbar = () => {
         <a href="/Home">Services</a>
         <a href="/Home">Careers</a>
         <a href="/Home">Contact</a>
+      </div>
+      <div>
+        <Button/>
       </div>
     </div>
   )
---
+++ lts.com/src/components/Work.jsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+const Work = () => {
+  return (
+    <div className="w-full h-screen bg-zinc-300">Work</div>
+  )
+}
+
+export default Work
